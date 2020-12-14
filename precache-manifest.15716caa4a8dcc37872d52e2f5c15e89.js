19f9b7e336d3414de468a67bf0e6156deef99d92
Kritika Ganesh Tripathi <kritikaktripathi@gmail.com>
2020-12-14 13:53:00 +0530

    Updates

---
+++ precache-manifest.15716caa4a8dcc37872d52e2f5c15e89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc8aba3e81b373f11c75",
+    "url": "/Tripathikritika/Todo/static/css/main.fbd7790a.chunk.css"
+  },
+  {
+    "revision": "bc8aba3e81b373f11c75",
+    "url": "/Tripathikritika/Todo/static/js/main.803d70d9.chunk.js"
+  },
+  {
+    "revision": "c9ee2302c5994dd6d4d7",
+    "url": "/Tripathikritika/Todo/static/js/runtime~main.9ef728e6.js"
+  },
+  {
+    "revision": "18027b281e4595edd832",
+    "url": "/Tripathikritika/Todo/static/js/2.59fb6918.chunk.js"
+  },
+  {
+    "revision": "260f94f9ba78bfdaac9c9e8e89ade5c3",
+    "url": "/Tripathikritika/Todo/static/media/background.260f94f9.jpg"
+  },
+  {
+    "revision": "8d5c0146f79fd6e0ca063e92d5bc518a",
+    "url": "/Tripathikritika/Todo/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Tripathikritika/Todo/precache-manifest.15716caa4a8dcc37872d52e2f5c15e89.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Tripathikritika/Todo/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
